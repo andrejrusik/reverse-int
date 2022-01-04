6832bfb2a4901dd197f9deb4b53e0de08ef3f0ba
Andrei Rushnitski <andrejrusik@gmail.com>
2022-01-04 21:29:10 +0100

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let enterNum = n < 0 ? n * -1 + "" : n + "";
+    let result = "";
+
+    if (+enterNum.substring(enterNum.length - 1) === 0) {
+        enterNum = enterNum.substring(0, enterNum.length - 1);
+    }
+    for (let i = 0; i < enterNum.length; ) {
+        result += enterNum.substring(enterNum.length - 1);
+        enterNum = enterNum.substring(0, enterNum.length - 1);
+    }
+    return result;
+};
